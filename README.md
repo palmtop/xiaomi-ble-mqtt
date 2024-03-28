2c3c3f7e8478558789b66b59d309383596e43da7
Laszlo Csilling <laco@csilling.com>
2024-03-28 21:20:44 +0100

    Support for MJ_HT_V1 added

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
-# Your support
-<a href="https://www.buymeacoffee.com/Ua0JwY9" target="_blank"><img src="https://www.buymeacoffee.com/assets/img/custom_images/orange_img.png" alt="Buy Me A Coffee" style="height: 41px !important;width: 174px !important;box-shadow: 0px 3px 2px 0px rgba(190, 190, 190, 0.5) !important;-webkit-box-shadow: 0px 3px 2px 0px rgba(190, 190, 190, 0.5) !important;" ></a>
 
 # About
 This is simple python script, which scans Xiaomi BLE Temperature and Humidity sensors and publishes measurements to MQTT. 
+
+It supports LYWSD03MMC and LYWSDCGQ(MJ_HT_V1) sensors, can be easily extended to LYWSD03.
 
 # Installation
 
@@ -10,6 +10,7 @@ This is simple python script, which scans Xiaomi BLE Temperature and Humidity se
     
     sudo pip3 install bluepy
     sudo pip3 install paho-mqtt
+    sudo pip3 install lywsd03mmc
     
 1.1.If you're using Ubuntu 20.04 on Raspberry Pi:
 
--- data-read.py
+++ data-read.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+import sys
 from mitemp.mitemp_bt.mitemp_bt_poller import MiTempBtPoller
 from mitemp.mitemp_bt.mitemp_bt_poller import MI_TEMPERATURE, MI_HUMIDITY, MI_BATTERY
 from btlewrap.bluepy import BluepyBackend
@@ -10,6 +11,7 @@ import configparser
 import os
 import json
 import datetime
+from lywsd03mmc import Lywsd03mmcClient
 
 workdir = os.path.dirname(os.path.realpath(__file__))
 config = configparser.ConfigParser()
@@ -24,60 +26,27 @@ averages.read("{0}/averages.ini".format(workdir))
 messages = []
 
 for device in devices:
-
     mac = config[device].get("device_mac")
-    poller = MiTempBtPoller(mac, BluepyBackend, ble_timeout=config[device].getint("timeout", 10))
-
     try:
+        if mac.startswith('A4:C1:38'):
+            client = Lywsd03mmcClient(mac)
+            #try:
+            sensor_data = client.data
+            data = json.dumps({
+                "temperature": sensor_data.temperature,
+                "humidity": sensor_data.humidity,
+                "battery": sensor_data.battery
+            })
+        else: 
+            poller = MiTempBtPoller(mac, BluepyBackend, ble_timeout=config[device].getint("timeout", 10))
+            temperature = poller.parameter_value(MI_TEMPERATURE)
+            humidity = poller.parameter_value(MI_HUMIDITY)
+            battery = poller.parameter_value(MI_BATTERY)
 
-        temperature = poller.parameter_value(MI_TEMPERATURE)
-        humidity = poller.parameter_value(MI_HUMIDITY)
-        battery = poller.parameter_value(MI_BATTERY)
-
-        data = json.dumps({
-            "temperature": temperature,
-            "humidity": humidity,
-            "battery": battery
-        })
-
-        # Check averages
-        avg = []
-        average_count = config[device].getint("average")
-        if average_count:
-            if mac in averages.sections():
-                avg = json.loads(averages[mac]["avg"])
-
-            # Add average
-            avg.insert(0, data)
-
-            # Strip data
-            avg = avg[0:average_count]
-
-            # Calc averages
-            temperature = 0
-            humidity = 0
-            battery = 0
-
-            for a in avg:
-                al = json.loads(a)
-                temperature += al["temperature"]
-                humidity += al["humidity"]
-                battery += al["battery"]
-
-            temperature = round(temperature / len(avg), 1)
-            humidity = round(humidity / len(avg), 1)
-            battery = round(battery / len(avg), 1)
-
-            # Convert averages
-            averages[mac] = {}
-            averages[mac]["avg"] = json.dumps(avg)
-
-            # Rewrite data
             data = json.dumps({
                 "temperature": temperature,
                 "humidity": humidity,
-                "battery": int(battery),
-                "average": len(avg)
+                "battery": battery
             })
 
         print(datetime.datetime.now(), device, " : ", data)
@@ -92,8 +61,9 @@ for device in devices:
         # print(traceback.print_exc())
     finally:
         messages.append({'topic': config[device].get("availability_topic"), 'payload': availability, 'retain': config[device].getboolean("retain", False)})
+    print(datetime.datetime.now(), device, " : ", messages)
 
-
+sys.exit()
 # Init MQTT
 mqtt_config = configparser.ConfigParser()
 mqtt_config.read("{0}/mqtt.ini".format(workdir))
